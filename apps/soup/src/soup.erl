440ede9798b7c98b83fca8f000923c4d45e0f301
aaronlelevier <aaron.lelevier@gmail.com>
2021-05-30 06:50:02 -0700

    soup:find/2 works for finding 'title'

--- apps/soup/src/soup.erl
+++ apps/soup/src/soup.erl
@@ -7,5 +7,44 @@
 -module(soup).
 -author("Aaron Lelevier").
 -vsn(1.0).
+
 -export([]).
+%% debug
+-compile(export_all).
+
+
+%% Macros
+
+-ifdef(debug_flag).
+-define(LOG, fun(X) -> io:format("Mod:~p Line:~p ~p~n", [?MODULE, ?LINE, X]) end).
+-else.
+-define(LOG(X), void).
+-endif.
+
+%% API
+
+find({Name, _Attrs, _Content} = Elem, Name) ->
+  ?LOG({Elem, Name}),
+  Elem;
+
+find({_OtherName, _Attrs, Content} = Elem, Name) when is_tuple(Content) orelse is_list(Content) ->
+  ?LOG({Elem, Name}),
+  find(Content, Name);
+
+find({_OtherName, _Attrs, _Content} = Elem, Name) ->
+  ?LOG({Elem, Name}),
+  no_match;
+
+find([H | T] = Elem, Name) ->
+  ?LOG({Elem, Name}),
+  case find(H, Name) of
+    no_match ->
+      find(T, Name);
+    Match ->
+      Match
+  end;
+
+find([] = Elem, Name) ->
+  ?LOG({Elem, Name}),
+  no_match.
 
